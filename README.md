842b9061a72ad7891934d72bfb4fff1a3b936106
abhishekkrthakur <abhishek4@gmail.com>
2020-04-27 15:52:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# YAWML: Yet Another Wrapper For Machine Learning
+pronounced as: yaw-m-l
+
+yawml is work in progress.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+import yawml
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="yawml",
+        version=sandesh.__version__,
+        description="YAWML: Yet Another Wrapper for Machine Learning",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author="Abhishek Thakur",
+        author_email="abhishek4@gmail.com",
+        url="https://github.com/abhishekkrthakur/yawml",
+        license="MIT License",
+        packages=find_packages(),
+        include_package_data=True,
+        platforms=["linux", "unix"],
+        python_requires='>3.5.2'
+    )
