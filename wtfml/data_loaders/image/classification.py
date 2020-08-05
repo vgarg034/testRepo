3d93b8aaa8c39bc54131f9f1b4d7e2bee7868702
abhishekkrthakur <abhishek4@gmail.com>
2020-08-05 18:37:04 +0200

    add opencv backend for reading images

--- wtfml/data_loaders/image/classification.py
+++ wtfml/data_loaders/image/classification.py
@@ -1,7 +1,7 @@
 """
 __author__: Abhishek Thakur
 """
-
+import cv2
 import torch
 
 import numpy as np
@@ -20,7 +20,15 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 class ClassificationDataset:
-    def __init__(self, image_paths, targets, resize, augmentations=None):
+    def __init__(
+        self,
+        image_paths,
+        targets,
+        resize,
+        augmentations=None,
+        backend="pil",
+        channel_first=True,
+    ):
         """
         :param image_paths: list of paths to images
         :param targets: numpy array
@@ -31,22 +39,42 @@ class ClassificationDataset:
         self.targets = targets
         self.resize = resize
         self.augmentations = augmentations
+        self.backend = backend
+        self.channel_first = channel_first
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, item):
-        image = Image.open(self.image_paths[item])
         targets = self.targets[item]
-        if self.resize is not None:
-            image = image.resize(
-                (self.resize[1], self.resize[0]), resample=Image.BILINEAR
-            )
-        image = np.array(image)
-        if self.augmentations is not None:
-            augmented = self.augmentations(image=image)
+        if self.backend == "pil":
+            image = Image.open(self.image_paths[item])
+            if self.resize is not None:
+                image = image.resize(
+                    (self.resize[1], self.resize[0]), resample=Image.BILINEAR
+                )
+            image = np.array(image)
+            if self.augmentations is not None:
+                augmented = self.augmentations(image=image)
+                image = augmented["image"]
+            if self.channel_first:
+                image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        elif self.backend == "cv2":
+            image = cv2.imread(self.image_paths[item])
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            if self.resize is not None:
+                image = cv2.resize(
+                    image,
+                    (self.resize[1], self.resize[0]),
+                    interpolation=cv2.INTER_CUBIC,
+                )
+            if self.augmentations is not None:
+                augmented = self.augmentations(image=image)
             image = augmented["image"]
-        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+            if self.channel_first:
+                image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        else:
+            raise Exception("Backend not implemented")
         return {
             "image": torch.tensor(image),
             "targets": torch.tensor(targets),
@@ -54,7 +82,15 @@ class ClassificationDataset:
 
 
 class ClassificationDataLoader:
-    def __init__(self, image_paths, targets, resize, augmentations=None):
+    def __init__(
+        self,
+        image_paths,
+        targets,
+        resize,
+        augmentations=None,
+        backend="pil",
+        channel_first=True,
+    ):
         """
         :param image_paths: list of paths to images
         :param targets: numpy array
@@ -65,11 +101,15 @@ class ClassificationDataLoader:
         self.targets = targets
         self.resize = resize
         self.augmentations = augmentations
+        self.backend = backend
+        self.channel_first = channel_first
         self.dataset = ClassificationDataset(
             image_paths=self.image_paths,
             targets=self.targets,
             resize=self.resize,
             augmentations=self.augmentations,
+            backend=self.backend,
+            channel_first=self.channel_first,
         )
 
     def fetch(self, batch_size, num_workers, drop_last=False, shuffle=True, tpu=False):
